bc8857941a8846d078f9aa0072c9b4be775d1842
balavishnu3333 <43338246+balavishnu3333@users.noreply.github.com>
2018-09-18 11:57:10 +0530

    Create pn.c

---
+++ pn.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+printf("Input:");
+scanf("%d",&i);
+if(i>0)
+{
+printf("Output: \n positive");
+}
+else
+{
+printf("Output: \n negative");
+}
+getch();
+}
