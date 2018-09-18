a112ed6d9f57d4a0834c7735d3618ac4149664e3
balavishnu3333 <43338246+balavishnu3333@users.noreply.github.com>
2018-09-18 12:11:11 +0530

    Create eo.c

---
+++ eo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+clrscr();
+printf("Input:");
+scanf("%d",&i);
+if(i%2==0)
+{
+printf("Output: \n EVEN");
+}
+else
+{
+printf("Output: \n invalid");
+}
+getch();
+}
