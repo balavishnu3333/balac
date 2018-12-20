b2d74117d8189ff7fe751bc01267d16a957875b9
balavishnu3333 <43338246+balavishnu3333@users.noreply.github.com>
2018-12-20 14:40:36 +0530

    9.c

---
+++ 9.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int N,K,n[5],i,sum;
+printf("Enter the no of values:");
+scanf("%d",&N);
+printf("Enter the no of values:");
+scanf("%d",&K);
+printf("Enter the values:");
+for(i=1;i<=5;i++)
+{
+scanf("%d",&n[i]);
+}
+
+for(i=0;i<=K;i++)
+{
+sum=sum+i;
+}
+printf("sum of K:%d",sum);
+}
