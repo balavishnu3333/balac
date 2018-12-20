e1f323f740212bb9ab35c86172cc9e00ebf3b6ee
balavishnu3333 <43338246+balavishnu3333@users.noreply.github.com>
2018-12-20 14:03:16 +0530

    suminteger.c

---
+++ suminteger.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,sum=0;
+clrscr();
+printf("Enter the positive integer:");
+scanf("%d",&n);
+for(i=1;i<=sum;i++)
+{
+sum +=i;
+}
+printf("SUM =%d",sum);
+getch();
+}
