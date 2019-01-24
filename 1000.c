489e9e06115bfb600a1e263035e190ccda0d85d9
balavishnu3333 <43338246+balavishnu3333@users.noreply.github.com>
2019-01-24 14:43:30 +0530

    Create 1000.c

---
+++ 1000.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n, reversedInteger = 0, remainder, originalInteger;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    originalInteger = 1000;
+    while( n!=0 )
+    {
+        remainder = n%10;
+        reversedInteger = reversedInteger*10 + remainder;
+        n /= 10;
+    }
+    if (originalInteger == reversedInteger)
+        printf("%d is a YES.", originalInteger);
+    else
+        printf("%d is a NO.", originalInteger);
+        return 0;
+}
